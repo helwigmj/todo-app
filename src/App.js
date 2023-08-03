0cf1215341fa33795ed1d3a51b49241fe830d775
Megan Helwig <helwigmegan@gmail.com>
2023-08-03 12:43:42 -0400

    remove filterButton component & functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
 import Form from './components/Form';
-import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
-
-const FILTER_MAP = {
-  All: () => true,
-  Active: (task) => !task.completed,
-  Completed: (task) => task.completed,
-};
-const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   // variables
   const [tasks, setTasks] = useState(props.tasks);
-  const [filter, setFilter] = useState("All");
   const taskList = tasks
-    .filter(FILTER_MAP[filter])
     .map((task) => (
       <Todo
         id={task.id}
@@ -27,14 +17,6 @@ function App(props) {
         deleteTask={deleteTask}
         editTask={editTask}
       />
-  ));
-  const filterList = FILTER_NAMES.map((name) => (
-    <FilterButton
-      key={name}
-      name={name}
-      isPressed={name === filter}
-      setFilter={setFilter} 
-    />
   ));
   const taskNoun = taskList.length !== 1 ? "tasks" : "task";
   const listHeadingText = `${taskList.length} ${taskNoun} remaining`;
@@ -77,9 +59,6 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
-      <div className="filters btn-group stack-exception">
-        {filterList}
-      </div>
       <h2 id="list-heading">{listHeadingText}</h2>
       <ul
         role="list"
